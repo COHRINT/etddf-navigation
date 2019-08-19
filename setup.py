d126c136e80fe35e79919067c71d7e4177af316d
Ian Loefgren <ian.loefgren@colorado.edu>
2019-08-19 13:54:10 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='etddf-navigation',
+        version='0.0.1',
+        description='Event-triggered decentralized data fusion based navigation filtering.',
+        author='Ian Loefgren',
+        author_email='ian.loefgren@colorado.edu',
+        py_modules= ['etddf_navigation'])
